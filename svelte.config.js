1e7ec976a484f8cc94dbf20cf9bf4c4b1b2cc20c
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-07-30 16:39:06 +0500

    add tailwind and melt

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+// Melt UI preprocessor
+import { preprocessMeltUI } from '@melt-ui/pp'
+import sequence from 'svelte-sequential-preprocessor'
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: sequence([
+ 		vitePreprocess(),
+        preprocessMeltUI() 
+	]),
 	kit: {
 		adapter: adapter(),
 		alias: {
