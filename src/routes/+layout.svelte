eddf4a522a2d79b6a2e13bb027f07768b7be508c
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-07-31 19:33:02 +0500

    dark mode and formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import "../app.css";
+	import '../app.css';
 
-    import ThemeSwitcher from "$lib/components/ThemeSwitcher.svelte";
-    let darkMode: boolean = false;
+	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
+	let darkMode: boolean = false;
 </script>
-  
+
 <div class:dark={darkMode}>
-    <div class="h-full bg-slate-100 dark:bg-slate-600">
-        <ThemeSwitcher bind:checked={darkMode}/>
-        <slot/>
-    </div>
+	<div class="absolute h-full w-full bg-slate-100 dark:bg-slate-700">
+		<ThemeSwitcher bind:checked={darkMode} />
+		<slot />
+	</div>
 </div>
